5e241f8d852136d55f74627c37ee796d35441e44
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-03 19:18:26 +0900

    add event logs

--- src/main/java/peco2282/bcreborn/event/ClientModEvents.java
+++ src/main/java/peco2282/bcreborn/event/ClientModEvents.java
@@ -8,6 +8,8 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import peco2282.bcreborn.BCReborn;
 import peco2282.bcreborn.core.block.BCCoreBlocks;
 import peco2282.bcreborn.core.block.container.EngineIronScreen;
@@ -17,13 +19,19 @@ import peco2282.bcreborn.core.block.menu.BCCoreMenuTypes;
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
 @Mod.EventBusSubscriber(modid = BCReborn.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModEvents {
+  private static final Logger log = LoggerFactory.getLogger(ClientModEvents.class);
+
   @SubscribeEvent
   public static void onClientSetup(FMLClientSetupEvent event) {
+    log.trace("onClientSetup");
+    log.trace("Register MenuScreens");
     MenuScreens.register(BCCoreMenuTypes.STONE_ENGINE.get(), EngineStoneScreen::new);
     MenuScreens.register(BCCoreMenuTypes.IRON_ENGINE.get(), EngineIronScreen::new);
-
+    log.trace("Done");
+    log.trace("Register ItemBlockRenderTypes");
     ItemBlockRenderTypes.setRenderLayer(BCCoreBlocks.STONE_ENGINE.get(), RenderType.cutout());
     ItemBlockRenderTypes.setRenderLayer(BCCoreBlocks.IRON_ENGINE.get(), RenderType.cutout());
+    log.trace("Done");
     // Some client setup code
 //    LOGGER.info("HELLO FROM CLIENT SETUP");
 //    LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
