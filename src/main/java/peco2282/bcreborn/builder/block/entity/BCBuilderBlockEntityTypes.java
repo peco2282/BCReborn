bbe08363e1d59bbdcfe7512182ef5a7604b656fd
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-03 19:18:04 +0900

    add entity-block

--- src/main/java/peco2282/bcreborn/builder/block/entity/BCBuilderBlockEntityTypes.java
+++ src/main/java/peco2282/bcreborn/builder/block/entity/BCBuilderBlockEntityTypes.java
@@ -4,12 +4,13 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.registries.RegistryObject;
 import peco2282.bcreborn.api.block.BCBlockEntity;
+import peco2282.bcreborn.builder.block.BCBuilderBlocks;
 import peco2282.bcreborn.registry.BCRegistry;
 
 import java.util.function.Supplier;
 
 public class BCBuilderBlockEntityTypes {
-  public static final RegistryObject<BlockEntityType<FillerBlockEntity>> FILLER = register("filler", () -> BlockEntityType.Builder.of(FillerBlockEntity::new).build(null));
+  public static final RegistryObject<BlockEntityType<FillerBlockEntity>> FILLER = register("filler", () -> BlockEntityType.Builder.of(FillerBlockEntity::new, BCBuilderBlocks.FILLER.get()).build(null));
 
   private static <T extends BlockEntity & BCBlockEntity> RegistryObject<BlockEntityType<T>> register(final String name, final Supplier<BlockEntityType<T>> type) {
     return BCRegistry.registerBlockEntityType(name, type);
