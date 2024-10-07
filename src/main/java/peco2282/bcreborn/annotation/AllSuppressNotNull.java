468d20febfba8930df392d2deac1efd01b48e63a
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-07 19:14:06 +0900

    add simple document

--- src/main/java/peco2282/bcreborn/annotation/AllSuppressNotNull.java
+++ src/main/java/peco2282/bcreborn/annotation/AllSuppressNotNull.java
@@ -1,12 +1,22 @@
 package peco2282.bcreborn.annotation;
 
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 import javax.annotation.meta.TypeQualifierDefault;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotate {MethodsReturnNonnullByDefault, FieldsAreNonnullByDefault, ParametersAreNonnullByDefault}
+ * @see MethodsReturnNonnullByDefault
+ * @see FieldsAreNonnullByDefault
+ * @see ParametersAreNonnullByDefault
+ */
 @Nonnull
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.PACKAGE, ElementType.TYPE})
--- src/main/java/peco2282/bcreborn/annotation/LateinitField.java
+++ src/main/java/peco2282/bcreborn/annotation/LateinitField.java
@@ -1,4 +1,7 @@
 package peco2282.bcreborn.annotation;
 
+/**
+ * Marker annotations for fields that are initially null, later accessed and thereafter non-null
+ */
 public @interface LateinitField {
 }
