96c3c7acb4f0ee6fe634f776cdb629b3295468a8
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-07 19:06:45 +0900

    move cap-event

--- src/main/java/peco2282/bcreborn/event/CapabilityEvent.java
+++ src/main/java/peco2282/bcreborn/event/CapabilityEvent.java
@@ -1,4 +1,4 @@
-package peco2282.bcreborn.api.mj;
+package peco2282.bcreborn.event;
 
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
@@ -6,17 +6,18 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import peco2282.bcreborn.BCReborn;
-import peco2282.bcreborn.lib.block.entity.TileContainerNeptune;
-import peco2282.bcreborn.lib.block.entity.TileNeptune;
+import peco2282.bcreborn.api.mj.MJCapability;
+import peco2282.bcreborn.api.mj.MJConnector;
 
 public class CapabilityEvent {
   private static final Logger log = LoggerFactory.getLogger(CapabilityEvent.class);
 
   @SubscribeEvent
   public static void onAttachCapabilities(AttachCapabilitiesEvent<BlockEntity> event) {
-    if (event.getObject() instanceof TileNeptune || event.getObject() instanceof TileContainerNeptune) {
-      log.trace(event.getObject().toString());
-      event.addCapability(BCReborn.location("mj"), new MJCapabilityHelper(new MJConnectorImpl(), (() -> 0), (l, s) -> 0));
+    BlockEntity entity = event.getObject();
+    if (entity instanceof MJConnector) {
+      log.trace("CapEvent {}", entity);
+//    event.addCapability(BCReborn.location(""), );
     }
   }
 }
