aab07045cdb0c5a6f77e3fc11a43f11ca7e7d6fb
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-10 18:10:28 +0900

    mj value atomic

--- src/main/java/peco2282/bcreborn/api/mj/MJHolder.java
+++ src/main/java/peco2282/bcreborn/api/mj/MJHolder.java
@@ -7,33 +7,34 @@ import net.minecraftforge.common.util.INBTSerializable;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class MJHolder implements INBTSerializable<CompoundTag> {
-  private final long capacity;
+  private final AtomicLong capacity = new AtomicLong(0);
   private final AtomicLong microJoules = new AtomicLong(0);
   public static final String CAP = "capacity";
   public static final String MJ = "current";
 
   public MJHolder(long capacity) {
-    this.capacity = capacity;
+    this.capacity.set(capacity);
   }
 
   @Override
   public CompoundTag serializeNBT(HolderLookup.Provider registryAccess) {
     var tag = new CompoundTag();
-    tag.putLong(CAP, capacity);
+    tag.putLong(CAP, capacity.get());
     tag.putLong(MJ, microJoules.get());
     return tag;
   }
 
   @Override
   public void deserializeNBT(HolderLookup.Provider registryAccess, CompoundTag nbt) {
-    microJoules.set(nbt.getLong(CAP));
+    capacity.set(nbt.getLong(CAP));
+    microJoules.set(nbt.getLong(MJ));
   }
 
   public long add(long mj) {
     if (isFull()) return 0;
-    if (current() + mj > capacity) {
-      var overflow = current() + mj - capacity;
-      microJoules.set(capacity);
+    if (current() + mj > capacity.get()) {
+      var overflow = current() + mj - capacity.get();
+      microJoules.set(capacity.get());
       return overflow;
     } else {
       microJoules.addAndGet(mj);
@@ -50,7 +51,7 @@ public class MJHolder implements INBTSerializable<CompoundTag> {
   }
 
   public boolean isFull() {
-    return microJoules.get() >= capacity;
+    return microJoules.get() >= capacity.get();
   }
 
   public boolean isEmpty() {
