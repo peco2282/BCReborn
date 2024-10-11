011229e1f695b3e57f6d7993abf22b0cdcbec2c6
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-11 17:49:28 +0900

    add QuarryBlock entity

---
+++ src/main/java/peco2282/bcreborn/builder/block/entity/QuarryBlockEntity.java
@@ -0,0 +1,11 @@
+package peco2282.bcreborn.builder.block.entity;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import peco2282.bcreborn.lib.block.entity.TileNeptune;
+
+public class QuarryBlockEntity extends TileNeptune {
+  public QuarryBlockEntity(BlockPos p_155229_, BlockState p_155230_) {
+    super(BCBuilderBlockEntityTypes.QUARRY.get(), p_155229_, p_155230_);
+  }
+}
