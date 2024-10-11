c46f48c6f6e7e118163dd38361ec107d4af513e4
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-11 17:54:08 +0900

    remove unused import

--- src/main/java/peco2282/bcreborn/builder/block/screen/widget/ModeChangeButton.java
+++ src/main/java/peco2282/bcreborn/builder/block/screen/widget/ModeChangeButton.java
@@ -2,18 +2,13 @@ package peco2282.bcreborn.builder.block.screen.widget;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.network.chat.Component;
 import peco2282.bcreborn.misc.ButtonTextureProvider;
 
 public class ModeChangeButton extends Button {
   public ModeChangeButton(int x, int y, int width, int height, Component message) {
-    super(x, y, width, height, message, b -> {}, DEFAULT_NARRATION);
-  }
-
-  @Override
-  public void onPress() {
-
+    super(x, y, width, height, message, b -> {
+    }, DEFAULT_NARRATION);
   }
 
   @Override
