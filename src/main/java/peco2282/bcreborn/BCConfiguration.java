a0dc66080974de91bf2a7b7c77c53d69cfc15628
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-14 15:52:08 +0900

    add marker_volume config

--- src/main/java/peco2282/bcreborn/BCConfiguration.java
+++ src/main/java/peco2282/bcreborn/BCConfiguration.java
@@ -8,19 +8,26 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = BCReborn.MODID)
 public class BCConfiguration {
   static class Holder {
-    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    private static final ForgeConfigSpec.Builder SPEC_BUILDER = new ForgeConfigSpec.Builder();
 
-    public static final ForgeConfigSpec.Builder CORE = BUILDER.push("core");
-    public static final ForgeConfigSpec.BooleanValue ENABLE_CREATIVE_ENGINE = BUILDER
+    public static final ForgeConfigSpec.Builder CORE = SPEC_BUILDER.push("core");
+    public static final ForgeConfigSpec.BooleanValue ENABLE_CREATIVE_ENGINE = CORE
         .comment(" Enable creative-engine")
         .define("enable_creative_engine", true);
+    public static final ForgeConfigSpec.IntValue MAX_VOLUME_BLOCK = CORE
+        .comment(" Volume torch between size 'blockA' to 'blockB'")
+        .defineInRange("max_volume_size", 32, 1, 32);
+    public static final ForgeConfigSpec.Builder BUILDER = SPEC_BUILDER.push("builder");
+    public static final ForgeConfigSpec.Builder TRANSPORT = SPEC_BUILDER.push("transport");
   }
-  static final ForgeConfigSpec SPEC = Holder.BUILDER.build();
+  static final ForgeConfigSpec SPEC = Holder.SPEC_BUILDER.build();
 
   public static boolean enableCreativeEngine;
+  public static int maxVolumeLength;
 
   @SubscribeEvent
   public static void onLoadConfig(ModConfigEvent event) {
     enableCreativeEngine = Holder.ENABLE_CREATIVE_ENGINE.get();
+    maxVolumeLength = Holder.MAX_VOLUME_BLOCK.get();
   }
 }
