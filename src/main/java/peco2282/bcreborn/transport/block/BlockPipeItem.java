d59985095226e1bd5bffd58a0a8f9b304e63e5a8
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 19:51:59 +0900

    pick getPipeType()

--- src/main/java/peco2282/bcreborn/transport/block/BlockPipeItem.java
+++ src/main/java/peco2282/bcreborn/transport/block/BlockPipeItem.java
@@ -2,11 +2,16 @@ package peco2282.bcreborn.transport.block;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import peco2282.bcreborn.transport.block.entity.BCTransportBlockEntities;
+import peco2282.bcreborn.transport.block.entity.ItemPipeBlockEntity;
 
 public class BlockPipeItem extends BaseBlockPipe {
   public BlockPipeItem(Properties properties, @NotNull String id, PipeMaterial material) {
@@ -18,13 +23,13 @@ public class BlockPipeItem extends BaseBlockPipe {
   }
 
   @Override
-  protected PipeType getPipeType() {
-    return PipeType.ITEM;
+  public @NotNull BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+    return new ItemPipeBlockEntity(pos, state, getPipeMaterial());
   }
 
   @Override
-  public @NotNull BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-    return null;
+  public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level p_153212_, BlockState p_153213_, BlockEntityType<T> p_153214_) {
+    return BaseEntityBlock.createTickerHelper(p_153214_, BCTransportBlockEntities.ITEM_PIPE.get(), ItemPipeBlockEntity::tick);
   }
 
   @Override
