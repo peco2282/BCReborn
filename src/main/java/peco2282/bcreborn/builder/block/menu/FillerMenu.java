510fd2a04ca603f833b2e46590123da59965ccac
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 18:09:16 +0900

    add resources

--- src/main/java/peco2282/bcreborn/builder/block/menu/FillerMenu.java
+++ src/main/java/peco2282/bcreborn/builder/block/menu/FillerMenu.java
@@ -12,8 +12,7 @@ import peco2282.bcreborn.api.enums.EnumFillerType;
 import peco2282.bcreborn.lib.block.menu.BCMenu;
 
 public class FillerMenu extends BCMenu {
-  private final Container input = new SimpleContainer(9);
-  private final Container result = new SimpleContainer(9);
+  private final Container type = new TypeConatiner();
   private final Container resource = new SimpleContainer(27);
   private EnumFillerType current;
   public FillerMenu(int id, Inventory inventory, @Nullable FriendlyByteBuf buffer) {
@@ -22,7 +21,7 @@ public class FillerMenu extends BCMenu {
 
     for (int sy = 0; sy < 3; sy++) {
       for (int sx = 0; sx < 9; sx++) {
-        addSlot(new Slot(resource, sx + sy * 9, sx * 18 + 8, sy * 18 + 40));
+        addSlot(new Slot(resource, sx + sy * 9, sx * 18 + 8, sy * 18 + 60));
       }
     }
   }
@@ -37,10 +36,48 @@ public class FillerMenu extends BCMenu {
     return this.inventory.stillValid(p_38874_);
   }
 
-  private void slotCheck() {
-    EnumFillerType.check(resource).ifPresent(type -> {
-      this.current = type;
-      result.setItem(0, type.getPanel().getDefaultInstance());
-    });
+  private static class TypeConatiner implements Container {
+    @Override
+    public int getContainerSize() {
+      return 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+      return false;
+    }
+
+    @Override
+    public ItemStack getItem(int p_18941_) {
+      return null;
+    }
+
+    @Override
+    public ItemStack removeItem(int p_18942_, int p_18943_) {
+      return null;
+    }
+
+    @Override
+    public ItemStack removeItemNoUpdate(int p_18951_) {
+      return null;
+    }
+
+    @Override
+    public void setItem(int p_18944_, ItemStack p_18945_) {
+
+    }
+
+    @Override
+    public void setChanged() {
+    }
+
+    @Override
+    public boolean stillValid(Player p_18946_) {
+      return true;
+    }
+
+    @Override
+    public void clearContent() {
+    }
   }
 }
