fbf6b4d109d8272346a158c8b2f920f931874a4c
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 18:07:10 +0900

    add blocktags

--- src/main/java/peco2282/bcreborn/data/tag/BCBlockTag.java
+++ src/main/java/peco2282/bcreborn/data/tag/BCBlockTag.java
@@ -7,7 +7,17 @@ import peco2282.bcreborn.BCReborn;
 
 @SuppressWarnings("SameParameterValue")
 public class BCBlockTag {
+  // Type
   public static final TagKey<Block> ENGINE = create("engine");
+  public static final TagKey<Block> PIPE = create("pipe");
+  public static final TagKey<Block> ITEM_PIPE = create("item_pipe");
+  public static final TagKey<Block> FLUID_PIPE = create("fluid_pipe");
+  public static final TagKey<Block> ENERGY_PIPE = create("energy_pipe");
+
+  // Package
+  public static final TagKey<Block> CORE = create("core");
+  public static final TagKey<Block> BUILDER = create("builder");
+  public static final TagKey<Block> TRANSPORT = create("transport");
 
   private static TagKey<Block> create(String name) {
     return BlockTags.create(BCReborn.MODID, name);
--- src/main/java/peco2282/bcreborn/data/tag/BCBlockTagsProvider.java
+++ src/main/java/peco2282/bcreborn/data/tag/BCBlockTagsProvider.java
@@ -6,7 +6,9 @@ import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 import peco2282.bcreborn.BCReborn;
+import peco2282.bcreborn.builder.block.BCBuilderBlocks;
 import peco2282.bcreborn.core.block.BCCoreBlocks;
+import peco2282.bcreborn.transport.block.BCTransportBlocks;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -22,5 +24,47 @@ public class BCBlockTagsProvider extends BlockTagsProvider {
         .add(BCCoreBlocks.STONE_ENGINE.get())
         .add(BCCoreBlocks.IRON_ENGINE.get())
         .add(BCCoreBlocks.CREATIVE_ENGINE.get());
+
+    tag(BCBlockTag.ITEM_PIPE)
+        .add(BCTransportBlocks.WOOD_ITEM_PIPE.get())
+        .add(BCTransportBlocks.STONE_ITEM_PIPE.get())
+        .add(BCTransportBlocks.COBBLESTONE_ITEM_PIPE.get())
+        .add(BCTransportBlocks.IRON_ITEM_PIPE.get())
+        .add(BCTransportBlocks.GOLD_ITEM_PIPE.get())
+        .add(BCTransportBlocks.DIAMOND_ITEM_PIPE.get());
+
+    tag(BCBlockTag.FLUID_PIPE)
+        .add(BCTransportBlocks.WOOD_FLUID_PIPE.get())
+        .add(BCTransportBlocks.STONE_FLUID_PIPE.get())
+        .add(BCTransportBlocks.COBBLESTONE_FLUID_PIPE.get())
+        .add(BCTransportBlocks.IRON_FLUID_PIPE.get())
+        .add(BCTransportBlocks.GOLD_FLUID_PIPE.get())
+        .add(BCTransportBlocks.DIAMOND_FLUID_PIPE.get());
+
+    tag(BCBlockTag.ENERGY_PIPE)
+        .add(BCTransportBlocks.WOOD_ENERGY_PIPE.get())
+        .add(BCTransportBlocks.STONE_ENERGY_PIPE.get())
+        .add(BCTransportBlocks.COBBLESTONE_ENERGY_PIPE.get())
+        .add(BCTransportBlocks.IRON_ENERGY_PIPE.get())
+        .add(BCTransportBlocks.GOLD_ENERGY_PIPE.get())
+        .add(BCTransportBlocks.DIAMOND_ENERGY_PIPE.get());
+
+    tag(BCBlockTag.PIPE)
+        .addTag(BCBlockTag.ITEM_PIPE)
+        .addTag(BCBlockTag.FLUID_PIPE)
+        .addTag(BCBlockTag.ENERGY_PIPE);
+
+    tag(BCBlockTag.CORE)
+        .addTag(BCBlockTag.ENGINE)
+        .add(BCCoreBlocks.SPRING.get())
+        .add(BCCoreBlocks.DECORATED.get())
+        .add(BCCoreBlocks.MARKER_VOLUME.get());
+
+    tag(BCBlockTag.BUILDER)
+        .add(BCBuilderBlocks.FILLER.get())
+        .add(BCBuilderBlocks.QUARRY.get());
+
+    tag(BCBlockTag.TRANSPORT)
+        .addTag(BCBlockTag.PIPE);
   }
 }
