395f6af89a78c30b11993b2fa1ab3721abe3387f
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 19:54:18 +0900

    add method for tick action

--- src/main/java/peco2282/bcreborn/transport/block/entity/ItemPipeBlockEntity.java
+++ src/main/java/peco2282/bcreborn/transport/block/entity/ItemPipeBlockEntity.java
@@ -1,6 +1,7 @@
 package peco2282.bcreborn.transport.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import peco2282.bcreborn.transport.block.BaseBlockPipe;
 
@@ -9,6 +10,12 @@ public class ItemPipeBlockEntity extends BasePipeBlockEntity {
   public ItemPipeBlockEntity(BlockPos p_155229_, BlockState p_155230_, BaseBlockPipe.PipeMaterial material) {
     super(BCTransportBlockEntities.ITEM_PIPE.get(), p_155229_, p_155230_);
     this.material = material;
+  }
+
+  private void update(BlockPos pos, BlockState state) {}
+
+  public static void tick(Level world, BlockPos pos, BlockState state, ItemPipeBlockEntity blockEntity) {
+    blockEntity.update(pos, state);
   }
 
   @Override
