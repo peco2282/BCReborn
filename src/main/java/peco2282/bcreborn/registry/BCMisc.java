4ac565350d4532aa5fbaa4ab460ae0a71931c389
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 18:06:47 +0900

    add creative tabs

--- src/main/java/peco2282/bcreborn/registry/BCMisc.java
+++ src/main/java/peco2282/bcreborn/registry/BCMisc.java
@@ -1,27 +1,34 @@
 package peco2282.bcreborn.registry;
 
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.RegistryObject;
 import peco2282.bcreborn.BCReborn;
-
-import java.util.Map;
+import peco2282.bcreborn.data.tag.BCBlockTag;
+import peco2282.bcreborn.data.tag.BCItemTag;
+import peco2282.bcreborn.utils.RegistryUtil;
 
 public class BCMisc {
-  public static final RegistryObject<CreativeModeTab> TAB = BCRegistry
-      .registerTab(BCReborn.MODID, () -> CreativeModeTab.builder()
+  public static final RegistryObject<CreativeModeTab> CORE_TAB = BCRegistry
+      .registerTab(name("core"), () -> CreativeModeTab.builder()
           .displayItems((params, output) -> {
-            ForgeRegistries.BLOCKS.getEntries()
-                .stream().filter(e -> e.getKey().location().getNamespace().equals(BCReborn.MODID))
-                .map(Map.Entry::getValue)
-                .forEach(output::accept);
+            output.acceptAll(RegistryUtil.flattenBlock(BCBlockTag.CORE).stream().map(ItemStack::new).toList());
+            output.acceptAll(RegistryUtil.fromItemTag(BCItemTag.GEAR).stream().map(ItemStack::new).toList());
 
-            ForgeRegistries.ITEMS.getEntries()
-                .stream().filter(e -> e.getKey().location().getNamespace().equals(BCReborn.MODID))
-                .map(Map.Entry::getValue)
-                .forEach(output::accept);
           })
           .build());
+  public static final RegistryObject<CreativeModeTab> BUILDER_TAB = BCRegistry
+      .registerTab(name("builder"), () -> CreativeModeTab.builder()
+          .displayItems((params, output) -> output.acceptAll(RegistryUtil.flattenBlock(BCBlockTag.BUILDER).stream().map(ItemStack::new).toList()))
+          .build());
+  public static final RegistryObject<CreativeModeTab> TRANSPORT_TAB = BCRegistry
+      .registerTab(name("transport"), () -> CreativeModeTab.builder()
+          .displayItems((params, output) -> output.acceptAll(RegistryUtil.flattenBlock(BCBlockTag.TRANSPORT).stream().map(ItemStack::new).toList()))
+          .build());
+
+  private static String name(String name) {
+    return BCReborn.MODID + "_" + name;
+  }
 
   static void init() {
   }
