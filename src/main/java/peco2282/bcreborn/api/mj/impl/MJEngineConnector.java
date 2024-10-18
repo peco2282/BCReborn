739ee1e703299564bebfdeef6f99673514760bca
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-18 20:52:28 +0900

    add engine cap-1

---
+++ src/main/java/peco2282/bcreborn/api/mj/impl/MJEngineConnector.java
@@ -0,0 +1,19 @@
+package peco2282.bcreborn.api.mj.impl;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import peco2282.bcreborn.api.block.BCProperties;
+import peco2282.bcreborn.api.mj.MJConnector;
+
+public class MJEngineConnector implements MJConnector {
+  @Override
+  public boolean isActive(Level level, BlockPos pos, BlockState state) {
+    return state.getValue(BCProperties.ACTIVE);
+  }
+
+  @Override
+  public boolean canConnection() {
+    return true;
+  }
+}
