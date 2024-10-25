4f69656ca14c07ab510de415b6e4b21610135c53
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-10-25 20:22:34 +0900

    add wooden item pipe recipe

--- src/main/java/peco2282/bcreborn/data/BCRecipeProvider.java
+++ src/main/java/peco2282/bcreborn/data/BCRecipeProvider.java
@@ -3,10 +3,13 @@ package peco2282.bcreborn.data;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import peco2282.bcreborn.BCReborn;
 import peco2282.bcreborn.core.item.BCCoreItems;
+import peco2282.bcreborn.transport.block.BCTransportBlocks;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
@@ -16,8 +19,12 @@ public class BCRecipeProvider extends RecipeProvider {
     super(p_248933_, p_333797_);
   }
 
-  static ShapedRecipeBuilder shaped(Supplier<? extends Item> item) {
+  static ShapedRecipeBuilder shapedItem(Supplier<? extends Item> item) {
     return ShapedRecipeBuilder.shaped(RecipeCategory.MISC, item.get());
+  }
+
+  static ShapedRecipeBuilder shapedBlock(Supplier<? extends Block> block) {
+    return ShapedRecipeBuilder.shaped(RecipeCategory.MISC, block.get());
   }
 
   static ShapelessRecipeBuilder shapeless(Supplier<Item> item) {
@@ -27,17 +34,18 @@ public class BCRecipeProvider extends RecipeProvider {
   @Override
   protected void buildRecipes(RecipeOutput p_297267_) {
     gear(p_297267_);
+    pipe(p_297267_);
   }
 
   void gear(RecipeOutput output) {
-    shaped(BCCoreItems.GEAR_WOOD)
+    shapedItem(BCCoreItems.GEAR_WOOD)
         .define('#', Items.STICK)
         .pattern(" # ")
         .pattern("# #")
         .pattern(" # ")
         .unlockedBy("has_item", has(Items.STICK))
         .save(output, BCReborn.location("gear_wood"));
-    shaped(BCCoreItems.GEAR_STONE)
+    shapedItem(BCCoreItems.GEAR_STONE)
         .define('G', BCCoreItems.GEAR_WOOD.get())
         .define('#', Items.STONE)
         .pattern(" # ")
@@ -45,7 +53,7 @@ public class BCRecipeProvider extends RecipeProvider {
         .pattern(" # ")
         .unlockedBy("has_item", has(BCCoreItems.GEAR_WOOD.get()))
         .save(output, BCReborn.location("gear_stone"));
-    shaped(BCCoreItems.GEAR_IRON)
+    shapedItem(BCCoreItems.GEAR_IRON)
         .define('#', Items.IRON_INGOT)
         .define('G', BCCoreItems.GEAR_STONE.get())
         .pattern(" # ")
@@ -53,7 +61,7 @@ public class BCRecipeProvider extends RecipeProvider {
         .pattern(" # ")
         .unlockedBy("has_item", has(BCCoreItems.GEAR_STONE.get()))
         .save(output, BCReborn.location("gear_iron"));
-    shaped(BCCoreItems.GEAR_GOLD)
+    shapedItem(BCCoreItems.GEAR_GOLD)
         .define('#', Items.GOLD_INGOT)
         .define('G', BCCoreItems.GEAR_IRON.get())
         .pattern(" # ")
@@ -61,7 +69,7 @@ public class BCRecipeProvider extends RecipeProvider {
         .pattern(" # ")
         .unlockedBy("has_item", has(BCCoreItems.GEAR_IRON.get()))
         .save(output, BCReborn.location("gear_gold"));
-    shaped(BCCoreItems.GEAR_GOLD)
+    shapedItem(BCCoreItems.GEAR_GOLD)
         .define('#', Items.DIAMOND)
         .define('G', BCCoreItems.GEAR_DIAMOND.get())
         .pattern(" # ")
@@ -69,5 +77,15 @@ public class BCRecipeProvider extends RecipeProvider {
         .pattern(" # ")
         .unlockedBy("has_item", has(BCCoreItems.GEAR_GOLD.get()))
         .save(output, BCReborn.location("gear_diamond"));
+  }
+
+  void pipe(RecipeOutput output) {
+    shapedBlock(BCTransportBlocks.WOOD_ITEM_PIPE)
+        .define('G', Items.GLASS)
+        .define('P', ItemTags.PLANKS)
+        .pattern("PGP")
+        .unlockedBy("has_planks", has(ItemTags.PLANKS))
+        .unlockedBy("has_glass", has(Items.GLASS))
+        .save(output, BCReborn.location("wood_item_pipe"));
   }
 }
