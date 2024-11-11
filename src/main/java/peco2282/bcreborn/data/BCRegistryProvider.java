192d57f51abc7c2226d5e82fa16c3cbf52458e60
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-11-11 19:01:29 +0900

    add registry

--- src/main/java/peco2282/bcreborn/data/BCRegistryProvider.java
+++ src/main/java/peco2282/bcreborn/data/BCRegistryProvider.java
@@ -2,17 +2,25 @@ package peco2282.bcreborn.data;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import peco2282.bcreborn.BCReborn;
+import peco2282.bcreborn.lib.damage.LiquidDamageType;
 import peco2282.bcreborn.registry.MenuTextureRegistry;
+import peco2282.bcreborn.registry.levelgen.BCFeatures;
+import peco2282.bcreborn.registry.levelgen.BCPlacements;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class BCRegistryProvider extends DatapackBuiltinEntriesProvider {
   private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-      .add(MenuTextureRegistry.MENU_TEXTURE, MenuTextureRegistry::bootstrap);
+      .add(MenuTextureRegistry.MENU_TEXTURE, MenuTextureRegistry::bootstrap)
+      .add(Registries.CONFIGURED_FEATURE, BCFeatures::bootstrap)
+      .add(Registries.PLACED_FEATURE, BCPlacements::bootstrap)
+      .add(Registries.DAMAGE_TYPE, LiquidDamageType::bootstrap);
+
   /**
    * Constructs a new datapack provider which generates all registry objects
    * from the provided mods using the holder. All entries that need to be
