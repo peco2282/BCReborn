3a58b415feaf702e8b7a7c9d85832a78789f4dac
peco2282 <87461070+peco2282@users.noreply.github.com>
2024-11-11 09:15:11 +0900

    add fluid-type

--- src/main/java/peco2282/bcreborn/core/fluid/BCCoreFluids.java
+++ src/main/java/peco2282/bcreborn/core/fluid/BCCoreFluids.java
@@ -1,6 +1,7 @@
 package peco2282.bcreborn.core.fluid;
 
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.registries.RegistryObject;
 import peco2282.bcreborn.registry.BCRegistry;
 
@@ -12,8 +13,12 @@ public class BCCoreFluids {
   public static final RegistryObject<Fuel.Source> FUEL_SOURCE = register("fuel", Fuel.Source::new);
   public static final RegistryObject<Fuel.Flowing> FUEL_FLOWING = register("fuel_flowing", Fuel.Flowing::new);
 
+  public static final RegistryObject<FluidType> OIL = BCRegistry.registerFluidType("oil", () -> new FluidType(FluidType.Properties.create().canPushEntity(true).canDrown(false).canHydrate(true).density(7).canSwim(false).temperature(5)));
+  public static final RegistryObject<FluidType> FUEL = BCRegistry.registerFluidType("fuel", () -> new FluidType(FluidType.Properties.create().canPushEntity(true).canDrown(false).canHydrate(true).density(7).canSwim(false).temperature(5)));
+
   private static <F extends Fluid> RegistryObject<F> register(String name, Supplier<F> supplier) {
     return BCRegistry.registerFluid(name, supplier);
   }
+
   public static void init() {}
 }
