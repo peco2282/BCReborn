bfd116682582dcc0601b986686f00c243c8181eb
peco2282 <peco2282@peco2282.com>
2026-05-18 20:57:31 +0900

    Add custom registries for BCReborn components

---
+++ src/main/java/com/peco2282/bcreborn/api/registry/BCRegistryKeys.java
@@ -0,0 +1,31 @@
+package com.peco2282.bcreborn.api.registry;
+
+import com.peco2282.bcreborn.api.blueprints.Schematic;
+import com.peco2282.bcreborn.api.core.IWorldProperty;
+import com.peco2282.bcreborn.api.crops.ICropHandler;
+import com.peco2282.bcreborn.api.facades.IFacadeItem;
+import com.peco2282.bcreborn.api.filler.IFillerPattern;
+import com.peco2282.bcreborn.api.fuels.ICoolant;
+import com.peco2282.bcreborn.api.fuels.IFuel;
+import com.peco2282.bcreborn.api.fuels.ISolidCoolant;
+import com.peco2282.bcreborn.api.statements.IStatement;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+public class BCRegistryKeys {
+  public static final ResourceKey<Registry<Schematic>> SCHEMATIC = key("schematic");
+  public static final ResourceKey<Registry<IWorldProperty>> WORLD_PROPERTY = key("world_property");
+  public static final ResourceKey<Registry<ICropHandler>> CROP_HANDLER = key("crop_handler");
+  public static final ResourceKey<Registry<IFacadeItem>> FACADE_ITEM = key("facade_item");
+  public static final ResourceKey<Registry<IFillerPattern>> FILLER_PATTERNS = key("filler_patterns");
+  public static final ResourceKey<Registry<IFuel>> FUELS = key("fuels");
+  public static final ResourceKey<Registry<ICoolant>> COOLANT = key("coolants");
+  public static final ResourceKey<Registry<ISolidCoolant>> SOLID_COOLANT = key("solid_coolants");
+  public static final ResourceKey<Registry<IStatement>> STATEMENT = key("statement");
+
+
+  private static <T> ResourceKey<Registry<T>> key(String name) {
+    return ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath("buildcraftcore", name));
+  }
+}
---
+++ src/main/java/com/peco2282/bcreborn/common/event/BCRegistryEvent.java
@@ -0,0 +1,34 @@
+package com.peco2282.bcreborn.common.event;
+
+import com.peco2282.bcreborn.BCRebornCore;
+import com.peco2282.bcreborn.api.blueprints.Schematic;
+import com.peco2282.bcreborn.api.core.IWorldProperty;
+import com.peco2282.bcreborn.api.crops.ICropHandler;
+import com.peco2282.bcreborn.api.facades.IFacadeItem;
+import com.peco2282.bcreborn.api.filler.IFillerPattern;
+import com.peco2282.bcreborn.api.fuels.ICoolant;
+import com.peco2282.bcreborn.api.fuels.IFuel;
+import com.peco2282.bcreborn.api.fuels.ISolidCoolant;
+import com.peco2282.bcreborn.api.library.LibraryTypeHandler;
+import com.peco2282.bcreborn.api.registry.BCRegistryKeys;
+import com.peco2282.bcreborn.api.statements.IStatement;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.NewRegistryEvent;
+import net.minecraftforge.registries.RegistryBuilder;
+
+@Mod.EventBusSubscriber(modid = BCRebornCore.MODID)
+public class BCRegistryEvent {
+  @SubscribeEvent
+  public void onRegistryEvent(NewRegistryEvent event) {
+    event.create(new RegistryBuilder<Schematic>().setName(BCRegistryKeys.SCHEMATIC.location()));
+    event.create(new RegistryBuilder<IWorldProperty>().setName(BCRegistryKeys.WORLD_PROPERTY.location()));
+    event.create(new RegistryBuilder<ICropHandler>().setName(BCRegistryKeys.CROP_HANDLER.location()));
+    event.create(new RegistryBuilder<IFacadeItem>().setName(BCRegistryKeys.FACADE_ITEM.location()));
+    event.create(new RegistryBuilder<IFillerPattern>().setName(BCRegistryKeys.FILLER_PATTERNS.location()));
+    event.create(new RegistryBuilder<IFuel>().setName(BCRegistryKeys.FUELS.location()));
+    event.create(new RegistryBuilder<ICoolant>().setName(BCRegistryKeys.COOLANT.location()));
+    event.create(new RegistryBuilder<ISolidCoolant>().setName(BCRegistryKeys.SOLID_COOLANT.location()));
+    event.create(new RegistryBuilder<IStatement>().setName(BCRegistryKeys.STATEMENT.location()));
+  }
+}
