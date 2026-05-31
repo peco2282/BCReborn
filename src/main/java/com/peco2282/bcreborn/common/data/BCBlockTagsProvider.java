7ba95ec62435ca061be82f06637fbec5867c5f62
peco2282 <peco2282@peco2282.com>
2026-05-31 10:49:24 +0900

    feat(tags): add item and block tags for Builders, Fuel, Robotics, Silicon, and more
    - Created new `BCItemTagsProvider` class for managing item tags.
    - Added `BUILDERS`, `FUEL_BUCKET`, `CORE`, `ROBOTICS`, `SILICON`, `ENERGY`, `CHIPSET`, and `GEAR` tags for items.
    - Updated `BCBlockTagsProvider` with `BUILDERS`, `FACTORY`, `FUEL`, `LASER_TABLE`, `ROBOTICS`, `SILICON`, and `TRANSPORT` tags for blocks.

--- src/main/java/com/peco2282/bcreborn/common/data/BCBlockTagsProvider.java
+++ src/main/java/com/peco2282/bcreborn/common/data/BCBlockTagsProvider.java
@@ -1,8 +1,13 @@
 package com.peco2282.bcreborn.common.data;
 
+import com.peco2282.bcreborn.builders.BuildersBlock;
 import com.peco2282.bcreborn.common.data.tag.CommonBlockTags;
 import com.peco2282.bcreborn.core.BlocksCore;
 import com.peco2282.bcreborn.energy.BlocksEnergy;
+import com.peco2282.bcreborn.energy.FluidsEnergy;
+import com.peco2282.bcreborn.factory.FactoryBlocks;
+import com.peco2282.bcreborn.robotics.RoboticsBlocks;
+import com.peco2282.bcreborn.silicon.SiliconBlocks;
 import com.peco2282.bcreborn.transport.BlocksTransport;
 import com.peco2282.bcreborn.transport.pipe.PipeType;
 import net.minecraft.core.HolderLookup;
@@ -24,11 +29,27 @@ public class BCBlockTagsProvider extends BlockTagsProvider {
   protected void addTags(HolderLookup.Provider p_256380_) {
     engineTag();
     pipeTag();
+    laserTableTag();
+    fuelBucketTag();
 
-    tag(CommonBlockTags.CORE).add(BlocksCore.WOODEN_ENGINE.get(), BlocksCore.SPRING.get());
-    tag(CommonBlockTags.ENERGY).add(BlocksEnergy.STONE_ENGINE.get(), BlocksEnergy.IRON_ENGINE.get(), BlocksEnergy.CREATIVE_ENGINE.get());
-
+    tag(CommonBlockTags.BUILDERS).add(BuildersBlock.ARCHITECT.get(), BuildersBlock.BUILDER.get(), BuildersBlock.FRAME.get(), BuildersBlock.CONSTRUCTION_MARKER.get(), BuildersBlock.BLUEPRINT_LIBRARY.get(), BuildersBlock.QUARRY.get(), BuildersBlock.FILLER.get());
+    tag(CommonBlockTags.CORE)
+        .add(BlocksCore.WOODEN_ENGINE.get(), BlocksCore.SPRING.get(), BlocksCore.BLUE_MARKER.get(), BlocksCore.PATH_MARKER.get(), BlocksCore.BUILD_TOOL.get());
+    tag(CommonBlockTags.ENERGY).add(BlocksEnergy.STONE_ENGINE.get(), BlocksEnergy.IRON_ENGINE.get(), BlocksEnergy.CREATIVE_ENGINE.get()).addTag(CommonBlockTags.FUEL);
+    tag(CommonBlockTags.FACTORY)
+        .add(FactoryBlocks.AUTO_WORKBENCH.get(), FactoryBlocks.HOPPER.get(), FactoryBlocks.FLOOD_GATE.get(), FactoryBlocks.MINING_WELL.get(), FactoryBlocks.PLAIN_PIPE.get(), FactoryBlocks.PUMP.get(), FactoryBlocks.REFINERY.get(), FactoryBlocks.TANK.get());
+    tag(CommonBlockTags.ROBOTICS).add(RoboticsBlocks.REQUESTER.get(), RoboticsBlocks.ZONE_PLAN.get());
+    tag(CommonBlockTags.SILICON).addTag(CommonBlockTags.LASER_TABLE).add(SiliconBlocks.LASER.get(), SiliconBlocks.PACKAGER.get());
     tag(CommonBlockTags.TRANSPORT).addTag(CommonBlockTags.PIPES);
+  }
+
+  private void fuelBucketTag() {
+    tag(CommonBlockTags.FUEL)
+        .add(FluidsEnergy.OIL_BLOCK.get(), FluidsEnergy.FUEL_BLOCK.get());
+  }
+
+  private void laserTableTag() {
+    tag(CommonBlockTags.LASER_TABLE).add(SiliconBlocks.ASSEMBLY_TABLE.get(), SiliconBlocks.ADVANCED_CRAFTING_TABLE.get(), SiliconBlocks.INTEGRATION_TABLE.get(), SiliconBlocks.CHARGING_TABLE.get(), SiliconBlocks.PROGRAMMING_TABLE.get(), SiliconBlocks.STAMPING_TABLE.get());
   }
 
   void engineTag() {
---
+++ src/main/java/com/peco2282/bcreborn/common/data/BCItemTagsProvider.java
@@ -0,0 +1,55 @@
+package com.peco2282.bcreborn.common.data;
+
+import com.peco2282.bcreborn.common.data.tag.CommonItemTags;
+import com.peco2282.bcreborn.core.ItemsCore;
+import com.peco2282.bcreborn.energy.FluidsEnergy;
+import com.peco2282.bcreborn.robotics.RoboticsItems;
+import com.peco2282.bcreborn.silicon.SiliconItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class BCItemTagsProvider extends ItemTagsProvider {
+  public BCItemTagsProvider(PackOutput p_275343_, CompletableFuture<HolderLookup.Provider> p_275729_, CompletableFuture<TagLookup<Block>> p_275322_, String modId, @Nullable ExistingFileHelper existingFileHelper) {
+    super(p_275343_, p_275729_, p_275322_, modId, existingFileHelper);
+  }
+
+  @Override
+  protected void addTags(HolderLookup.Provider p_256380_) {
+    gearTag();
+    chipsetTag();
+    fuelBucketTag();
+
+    tag(CommonItemTags.BUILDERS);
+    tag(CommonItemTags.CORE)
+        .addTag(CommonItemTags.GEAR).add(ItemsCore.LIST.get(), ItemsCore.MAP_LOCATION.get(), ItemsCore.PAINTBRUSH.get(), ItemsCore.WRENCH.get());
+    tag(CommonItemTags.ENERGY)
+        .addTag(CommonItemTags.FUEL_BUCKET);
+    tag(CommonItemTags.FACTORY);
+    tag(CommonItemTags.ROBOTICS)
+        .add(RoboticsItems.REDSTONE_BOARD.get(), RoboticsItems.ROBOT.get(), RoboticsItems.ROBOT_STATION.get());;
+    tag(CommonItemTags.SILICON)
+        .addTag(CommonItemTags.CHIPSET).add(SiliconItems.PACKAGE_ITEM.get());
+    tag(CommonItemTags.TRANSPORT);
+  }
+
+  private void fuelBucketTag() {
+    tag(CommonItemTags.FUEL_BUCKET)
+        .add(FluidsEnergy.OIL_BUCKET.get(), FluidsEnergy.FUEL_BUCKET.get());
+  }
+
+  private void chipsetTag() {
+    tag(CommonItemTags.CHIPSET)
+        .add(SiliconItems.REDSTONE_CHIPSET.get(), SiliconItems.IRON_CHIPSET.get(), SiliconItems.GOLD_CHIPSET.get(), SiliconItems.DIAMOND_CHIPSET.get(), SiliconItems.PULSATING_CHIPSET.get(), SiliconItems.QUARTZ_CHIPSET.get(), SiliconItems.COMP_CHIPSET.get(), SiliconItems.EMERALD_CHIPSET.get());
+  }
+
+  private void gearTag() {
+    tag(CommonItemTags.GEAR)
+        .add(ItemsCore.WOODEN_GEAR.get(), ItemsCore.IRON_GEAR.get(), ItemsCore.GOLD_GEAR.get(), ItemsCore.DIAMOND_GEAR.get());
+  }
+}
