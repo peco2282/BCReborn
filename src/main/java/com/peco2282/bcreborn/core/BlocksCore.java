760c2dae594ca3816f703368b507d20fb0c93975
peco2282 <peco2282@peco2282.com>
2026-05-31 10:48:45 +0900

    feat(blocks): add `BUILD_TOOL` block and update `PATH_MARKER` type definition

--- src/main/java/com/peco2282/bcreborn/core/BlocksCore.java
+++ src/main/java/com/peco2282/bcreborn/core/BlocksCore.java
@@ -3,10 +3,7 @@ package com.peco2282.bcreborn.core;
 import com.peco2282.bcreborn.BCRebornCore;
 import com.peco2282.bcreborn.common.BCRegistry;
 import com.peco2282.bcreborn.common.bean.InitRegister;
-import com.peco2282.bcreborn.core.block.BlueMarkerBlock;
-import com.peco2282.bcreborn.core.block.PathMarkerBlock;
-import com.peco2282.bcreborn.core.block.SpringBlock;
-import com.peco2282.bcreborn.core.block.WoodEngineBlock;
+import com.peco2282.bcreborn.core.block.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -17,9 +14,10 @@ public class BlocksCore {
   private static final BCRegistry REGISTRY = BCRebornCore.getRegistry();
 
   public static final RegistryObject<WoodEngineBlock> WOODEN_ENGINE = register("wood_engine", WoodEngineBlock::new);
-  public static final RegistryObject<Block> PATH_MARKER = register("path_marker", PathMarkerBlock::new);
+  public static final RegistryObject<PathMarkerBlock> PATH_MARKER = register("path_marker", PathMarkerBlock::new);
   public static final RegistryObject<SpringBlock> SPRING = register("eternal_spring", SpringBlock::new);
   public static final RegistryObject<BlueMarkerBlock> BLUE_MARKER = register("blue_marker", BlueMarkerBlock::new);
+  public static final RegistryObject<BuildToolBlock> BUILD_TOOL = register("build_tool", BuildToolBlock::new);
 
   private static <B extends Block> RegistryObject<B> register(String name, Supplier<B> block) {
     return REGISTRY.registerBlockItem(name, block);
