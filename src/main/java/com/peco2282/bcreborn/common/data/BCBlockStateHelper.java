8c02cf6b2e2e1b3be682fca47f7537e1bc9e53dd
peco2282 <peco2282@peco2282.com>
2026-06-03 17:02:38 +0900

    feat(data): add BCBlockStateHelper for streamlined block state and model generation

---
+++ src/main/java/com/peco2282/bcreborn/common/data/BCBlockStateHelper.java
@@ -0,0 +1,38 @@
+package com.peco2282.bcreborn.common.data;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.*;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+@SuppressWarnings("UnusedReturnValue")
+public abstract class BCBlockStateHelper extends BlockStateProvider {
+  private final ModelProvider<?> provider;
+  public BCBlockStateHelper(PackOutput output, String modid, ExistingFileHelper exFileHelper) {
+    super(output, modid, exFileHelper);
+    this.provider = models();
+  }
+
+  protected abstract void registerStatesAndModels();
+
+  protected ItemModelBuilder basicItem(Item item) {
+    return itemModels().basicItem(item);
+  }
+
+  protected BlockModelBuilder getBlockBuilder(String name) {
+    return models().getBuilder(name);
+  }
+
+  protected ItemModelBuilder getItemBuilder(String name) {
+    return itemModels().getBuilder(name);
+  }
+
+  protected ModelFile.ExistingModelFile getExistingFile(ResourceLocation name) {
+    return provider.getExistingFile(name);
+  }
+
+  protected ItemModelBuilder withExistingItemParent(String name, ResourceLocation parent) {
+    return itemModels().withExistingParent(name, parent);
+  }
+}
