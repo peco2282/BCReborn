8207546563c1f03466dae3d16c1604c85b44b31b
peco2282 <peco2282@peco2282.com>
2026-06-05 19:56:22 +0900

    feat(registry): add `SingleKey` support to `KeyedRegistryObject` for single-key mappings

--- src/main/java/com/peco2282/bcreborn/common/registry/KeyedRegistryObject.java
+++ src/main/java/com/peco2282/bcreborn/common/registry/KeyedRegistryObject.java
@@ -21,10 +21,20 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public abstract class KeyedRegistryObject<V> implements Supplier<RegistryObject<V>> {
   // @formatter:off
+  public static <V, K> SingleKey<V, K> single(
+    Collection<K> keys,                                             // Allow Key to be a collection
+    Function<K, String> nameMaker,                                  // Create registry name from key
+    BiFunction<String, Supplier<V>, RegistryObject<V>> registerer,  // Registerer the object
+    Function<K, V> constructor                                      // Constructor for the object
+  ) {
+    return new SingleKey<>(keys, nameMaker, registerer, constructor);
+  }
+
   public static <V, K1, K2> TwoKeys<V, K1, K2> two(
     Collection<K1> key1s,                                           // Allow Key1 to be a collection
     Collection<K2> key2s,                                           // Allow Key2 to be a collection
@@ -60,6 +70,43 @@ public abstract class KeyedRegistryObject<V> implements Supplier<RegistryObject<
   // @formatter:on
 
   public abstract List<RegistryObject<V>> getAll();
+
+  public static class SingleKey<V, K> extends KeyedRegistryObject<V> {
+    private final Collection<K> keys;
+
+    private final Map<K, RegistryObject<V>> map = new ConcurrentHashMap<>();
+
+    private SingleKey(
+      Collection<K> keys,
+      Function<K, String> nameMaker,
+      BiFunction<String, Supplier<V>, RegistryObject<V>> registerer,
+      Function<K, V> constructor
+    ) {
+      this.keys = keys;
+      for (K k : keys) {
+        String name = nameMaker.apply(k);
+        Supplier<V> supplier = () -> constructor.apply(k);
+        map.put(k, registerer.apply(name, supplier));
+      }
+    }
+    @Override
+    public List<RegistryObject<V>> getAll() {
+      return List.copyOf(map.values());
+    }
+
+    @Override
+    public RegistryObject<V> get() {
+      return map.get(keys.iterator().next());
+    }
+
+    public RegistryObject<V> get(K key) {
+      return map.get(key);
+    }
+
+    public Map<K, RegistryObject<V>> getMap() {
+      return Collections.unmodifiableMap(map);
+    }
+  }
 
   public static class TwoKeys<V, K1, K2> extends KeyedRegistryObject<V> {
     private final Collection<K1> key1s;
