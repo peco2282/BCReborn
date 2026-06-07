0fa6d84c2766663a74300a54db7d99df6e1b9f70
peco2282 <peco2282@peco2282.com>
2026-06-07 19:18:31 +0900

    refactor(blocks): add directional property to `QuarryBlock` and `BlueprintLibraryBlock` for placement-facing logic

--- src/main/java/com/peco2282/bcreborn/builders/block/BlueprintLibraryBlock.java
+++ src/main/java/com/peco2282/bcreborn/builders/block/BlueprintLibraryBlock.java
@@ -16,10 +16,12 @@ import com.peco2282.bcreborn.builders.block.entity.BlueprintLibraryBlockEntity;
 import com.peco2282.bcreborn.common.block.BuildCraftBlock;
 import com.peco2282.bcreborn.common.block.entity.BuildCraftBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -27,18 +29,26 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 public class BlueprintLibraryBlock extends BuildCraftBlock {
+  public static final DirectionProperty FACING = DirectionProperty.create("facing");
   public BlueprintLibraryBlock() {
     super(Properties.of().noOcclusion().lightLevel(state -> 0).strength(5.0F));
+    this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+  }
+
+  @Override
+  public @Nullable BlockState getStateForPlacement(BlockPlaceContext p_49820_) {
+    return super.getStateForPlacement(p_49820_).setValue(FACING, p_49820_.getHorizontalDirection().getOpposite());
   }
 
   @Override
   public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_49915_) {
-
+    p_49915_.add(FACING);
   }
 
   @Override
--- src/main/java/com/peco2282/bcreborn/builders/block/QuarryBlock.java
+++ src/main/java/com/peco2282/bcreborn/builders/block/QuarryBlock.java
@@ -16,6 +16,8 @@ import com.peco2282.bcreborn.builders.block.entity.QuarryBlockEntity;
 import com.peco2282.bcreborn.common.block.BuildCraftBlock;
 import com.peco2282.bcreborn.common.block.entity.BuildCraftBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -23,11 +25,19 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import org.jetbrains.annotations.Nullable;
 
 public class QuarryBlock extends BuildCraftBlock {
+  public static final DirectionProperty FACING = DirectionProperty.create("facing");
   public QuarryBlock() {
     super(Properties.of().noOcclusion().lightLevel(state -> 1));
+    this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.NORTH));
+  }
+
+  @Override
+  public @Nullable BlockState getStateForPlacement(BlockPlaceContext p_49820_) {
+    return super.getStateForPlacement(p_49820_).setValue(FACING, p_49820_.getHorizontalDirection().getOpposite());
   }
 
   @Nullable
@@ -38,7 +48,7 @@ public class QuarryBlock extends BuildCraftBlock {
 
   @Override
   public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_49915_) {
-
+    p_49915_.add(FACING);
   }
 
   @Override
