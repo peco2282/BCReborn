828f5123ccdedb6b0bde5ed9151809edbaee47c5
peco2282 <peco2282@peco2282.com>
2026-06-11 22:20:06 +0900

    feat(recipes): add recipe manager implementations including Assembly, Integration, Programming, Refinery, and core FlexibleRecipe classes

---
+++ src/main/java/com/peco2282/bcreborn/core/crops/CropHandlerPlantable.java
@@ -0,0 +1,99 @@
+package com.peco2282.bcreborn.core.crops;
+
+
+import com.peco2282.bcreborn.api.crops.ICropHandler;
+import com.peco2282.bcreborn.common.utils.BlockUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.*;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.IPlantable;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CropHandlerPlantable implements ICropHandler {
+
+	private static final Set<Block> FORBIDDEN_BLOCKS = new HashSet<Block>();
+
+	public static void forbidBlock(Block b) {
+		FORBIDDEN_BLOCKS.add(b);
+	}
+
+	@Override
+	public boolean isSeed(ItemStack stack) {
+		if (stack.getItem() instanceof IPlantable) {
+			return true;
+		}
+
+		if (stack.getItem() instanceof BlockItem item) {
+			Block block = item.getBlock();
+      return block instanceof IPlantable && !FORBIDDEN_BLOCKS.contains(block);
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean canSustainPlant(Level world, ItemStack seed, BlockPos pos) {
+		if (seed.getItem() instanceof IPlantable) {
+			return world.getBlockState(pos).canSustainPlant(world, pos, Direction.UP,
+					(IPlantable) seed.getItem())
+					&& world.getBlockState(pos.above()).isAir();
+		} else if (seed.getItem() instanceof BlockItem item && item.getBlock() instanceof IPlantable plantable) {
+			BlockState state = world.getBlockState(pos);
+			return state.canSustainPlant(world, pos, Direction.UP, plantable)
+					&& state.getBlock() != item.getBlock()
+					&& world.getBlockState(pos.above()).isAir();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean plantCrop(Level world, Player player, ItemStack seed, BlockPos pos) {
+		return seed.useOn(new UseOnContext(player, player.getUsedItemHand(), new BlockHitResult(Vec3.atCenterOf(pos), Direction.UP, pos, false))).consumesAction();
+	}
+
+	@Override
+	public boolean isMature(BlockGetter blockAccess, BlockState state, BlockPos pos) {
+		Block block = state.getBlock();
+		if (block == null || FORBIDDEN_BLOCKS.contains(block)) {
+			return false;
+		} else if (block instanceof TallGrassBlock
+				|| block instanceof MelonBlock
+				|| block instanceof MushroomBlock
+				|| block instanceof DoublePlantBlock
+				|| block == Blocks.PUMPKIN) {
+			return true;
+		} else if (block instanceof CropBlock crop) {
+			return crop.isMaxAge(state);
+		} else if (block instanceof NetherWartBlock) {
+			return state.getValue(NetherWartBlock.AGE) == 3;
+		} else if (block instanceof IPlantable) {
+			if (pos.getY() > 0 && blockAccess.getBlockState(pos.below()).is(block)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean harvestCrop(Level world, BlockPos pos, List<ItemStack> drops) {
+		if (!world.isClientSide) {
+			if (BlockUtils.breakBlock((ServerLevel) world, pos, drops)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/main/java/com/peco2282/bcreborn/core/crops/CropHandlerReeds.java
@@ -0,0 +1,46 @@
+package com.peco2282.bcreborn.core.crops;
+import com.peco2282.bcreborn.api.crops.CropManager;
+import com.peco2282.bcreborn.api.crops.ICropHandler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.IPlantable;
+
+import java.util.List;
+
+public class CropHandlerReeds implements ICropHandler {
+
+	@Override
+	public boolean isSeed(ItemStack stack) {
+		return stack.is(Items.SUGAR_CANE);
+	}
+
+	@Override
+	public boolean canSustainPlant(Level world, ItemStack seed, BlockPos pos) {
+		BlockState state = world.getBlockState(pos);
+		return state.canSustainPlant(world, pos, Direction.UP, (IPlantable) Blocks.SUGAR_CANE)
+				&& !state.is(Blocks.SUGAR_CANE)
+				&& world.getBlockState(pos.above()).isAir();
+	}
+
+	@Override
+	public boolean plantCrop(Level world, Player player, ItemStack seed, BlockPos pos) {
+		return CropManager.getDefaultHandler().plantCrop(world, player, seed, pos);
+	}
+
+	@Override
+	public boolean isMature(BlockGetter blockAccess, BlockState state, BlockPos pos) {
+		return false;
+	}
+
+	@Override
+	public boolean harvestCrop(Level world, BlockPos pos, List<ItemStack> drops) {
+		return false;
+	}
+}
