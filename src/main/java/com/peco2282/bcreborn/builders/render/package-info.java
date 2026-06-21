dc621eb4ff8dbad61250f32cdacd90fb00427606
peco2282 <peco2282@peco2282.com>
2026-06-21 15:09:02 +0900

    refactor(builders): remove unused render classes and associated package

--- src/main/java/com/peco2282/bcreborn/builders/render/package-info.java
+++
@@ -1,7 +0,0 @@
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package com.peco2282.bcreborn.builders.render;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
