a27499783bb73d175984b2169b5a1f8cc7d8b5c6
peco2282 <peco2282@peco2282.com>
2026-06-27 17:30:54 +0900

    feat(core): add package annotations for null safety in `block.render`

---
+++ src/main/java/com/peco2282/bcreborn/builders/block/render/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.peco2282.bcreborn.builders.block.render;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
